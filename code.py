d003c6b6d339060a3cfe4350673a3f5e29c41c90
TheHockeyist <thockeyist@gmail.com>
2017-02-16 14:09:17 -0600

    Initial code.
    A brief overview of how the program will work.

---
+++ code.py
@@ -0,0 +1,7 @@
+# Ask user for string to be transliterated to Cyrillic, including soft and hard signs.
+
+# Mechanism to search the string and replace letters with Cyrillic equivalents, e.g. d to д. For unclear situations (e.g. deciding between и and й), it will either make an educated guess based on the context or ask the user what Cyrillic letter was actually meant.
+
+# Check the Cyrillic transliteration for errors that break Russian orthography rules and fix them, e.g. цх ---> ч or йу ---> ю.
+
+#Print final result in copy-pasteable format.
