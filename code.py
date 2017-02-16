af68874d0416c3d78e5fb672079beebf7fb9b4a3
TheHockeyist <thockeyist@gmail.com>
2017-02-16 14:20:28 -0600

    Ask user for string.
    Next, I will need to replace the Latin letters with Cyrillic ones.

--- code.py
+++ code.py
@@ -1,6 +1,11 @@
 # Ask user for string to be transliterated to Cyrillic, including soft and hard signs.
 
+latin_string = raw_input("Ask user for something.")
+cyrillic_string = raw_input
+
 # Mechanism to search the string and replace letters with Cyrillic equivalents, e.g. d to д. For unclear situations (e.g. deciding between и and й), it will either make an educated guess based on the context or ask the user what Cyrillic letter was actually meant.
+
+#E.g. mechanism to replace d with д here as an example. Repeat for the other letters.
 
 # Check the Cyrillic transliteration for errors that break Russian orthography rules and fix them, e.g. цх ---> ч or йу ---> ю.
 
