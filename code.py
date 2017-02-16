a9be5b7d9607a0469b00429c35c547bf899cac7e
TheHockeyist <thockeyist@gmail.com>
2017-02-16 14:27:46 -0600

    Attempt to replace d with д.
    Not sure if this works yet.

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ cyrillic_string = raw_input
 
 # Mechanism to search the string and replace letters with Cyrillic equivalents, e.g. d to д. For unclear situations (e.g. deciding between и and й), it will either make an educated guess based on the context or ask the user what Cyrillic letter was actually meant.
 
-#E.g. mechanism to replace d with д here as an example. Repeat for the other letters.
+str.replace(d, д)
 
 # Check the Cyrillic transliteration for errors that break Russian orthography rules and fix them, e.g. цх ---> ч or йу ---> ю.
 
